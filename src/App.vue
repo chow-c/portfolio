a31833107d27f6cc3e9a8c18881bc6d2cb5dca0c
chow-c <18460362+chow-c@users.noreply.github.com>
2022-02-20 10:39:19 +1100

    :sparkles: Add project description views

--- src/App.vue
+++ src/App.vue
@@ -70,11 +70,17 @@
                       max-width="379"
                       justify="center"
                       hover
-                      v-bind:style="{
-                        transform: 'rotate(' + getRotation(idx, 2.5) + 'deg)',
-                      }"
+                      v-bind:style="[
+                        idx === selectedCard ? { elevation: 10 } : {},
+                        {
+                          transform: 'rotate(' + getRotation(idx, 2.5) + 'deg)',
+                        },
+                      ]"
                     >
-                      <v-responsive :aspect-ratio="0.83">
+                      <v-responsive
+                        :aspect-ratio="0.83"
+                        @click="selectedCard = idx"
+                      >
                         <v-img
                           :src="getImgUrl(item.img)"
                           class="mx-5 mt-5"
@@ -87,6 +93,28 @@
                           item.text
                         }}</v-card-subtitle>
                       </v-responsive>
+                      <v-expand-transition>
+                        <v-sheet
+                          v-if="selectedCard === idx"
+                          class="
+                            transition-fast-in-fast-out
+                            v-card--reveal
+                            pa-3
+                            d-flex
+                            flex-column
+                            justify-center
+                          "
+                          style="height: 100%"
+                          @click="selectedCard = null"
+                        >
+                          <v-card-text class="pb-0 text-center">
+                            <p class="text-h5 text--primary">
+                              {{ item.title }}
+                            </p>
+                            <p class="text-body-1">{{ item.description }}</p>
+                          </v-card-text>
+                        </v-sheet>
+                      </v-expand-transition>
                     </v-card>
                   </v-col>
                 </transition-group>
@@ -122,6 +150,7 @@ export default {
   components: {},
 
   data: () => ({
+    selectedCard: null,
     skills: [],
     socials: [
       {
@@ -258,5 +287,12 @@ export default {
 
 .v-image__image {
   border: 0.5px solid grey;
+}
+
+.v-card--reveal {
+  bottom: 0;
+  opacity: 1 !important;
+  position: absolute;
+  width: 100%;
 }
 </style>
